10088c304cb21fcdd13af82643ac7d4e91722c79
ocreamer888 <ocreamerdrive@gmail.com>
2025-10-29 00:47:48 -0600

    implement 3D object

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import ProductViewer from './components/ProductViewer'
 
 const App = () => {
   return (  
     <main>
     <Navbar />
     <Hero />
+    <ProductViewer />
     </main>
   )
 }
