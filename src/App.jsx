31c76c96041e8684cc3f4a4a4c1e6f60518dc7ee
ocreamer888 <ocreamerdrive@gmail.com>
2025-10-30 02:51:55 -0600

    Revert "showcase section"
    This reverts commit 403ed936118079ce7e9c6ccdf125e78a211d4456.
    
    e	modified:   src/App.jsx
    e

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import ProductViewer from './components/ProductViewer'
-import Showcase from './components/Showcase'
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
@@ -13,7 +12,6 @@ const App = () => {
     <Navbar />
     <Hero />
     <ProductViewer />
-    <Showcase />
     </main>
   )
 }
